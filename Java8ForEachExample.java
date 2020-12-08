929d81be3ba375bea9eeff997f8ece7539527986
rpn555 <41237522+rpn555@users.noreply.github.com>
2020-12-08 14:14:30 +0530

    Add files via upload

--- Java8ForEachExample.java
+++ Java8ForEachExample.java
@@ -19,6 +19,7 @@ public class Java8ForEachExample {
 		while(it.hasNext()){
 			Integer i = it.next();
 			System.out.println("Iterator Value::"+i);
+			System.out.println("Iterator Value::"+i);
 		}
 		
 		//traversing through forEach method of Iterable with anonymous class
